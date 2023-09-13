dc79dfbd9b900740743336aaad6a3bf95c3b1039
Jayanth Madupalli <jayanth.madupalli@gmail.com>
2023-09-13 14:41:39 -0500

    lc #20

---
+++ 02_valid_parantheses_lc20.py
@@ -0,0 +1,36 @@
+# LeetCode 20
+class Solution:
+    # @param1: string of parantheses
+    # return: bool, valid: True/False
+    def isValid(self, s: str) -> bool:
+        # maintain the list of all parantheses
+        braces = '({['
+        # maintain the map from open to close
+        cTo = {')': '(', '}': '{', ']': '['}
+        # surprise! this is a stack problem :)
+        stack = []
+        for i in range(len(s)):
+            # if current character is an open parantheses
+            # push it to stack
+            if s[i] in braces:
+                stack.append(s[i])
+            else:
+                # if not, it must be a close paranthesis
+                # problem states string only has parantheses
+                # if stack is empty at this point, we don't have a matching open
+                # so invalid
+                if len(stack) == 0:
+                    return False
+                # pop the top of the stack
+                top = stack.pop()
+                # see if it matches the current closing
+                if top != cTo[s[i]]:
+                    return False
+        # if there is a leftover in the stack, more open brackets (then also invalid)
+        if len(stack) != 0:
+            return False
+        # after handling all invalid cases, the string is valid!
+        return True
+
+    # time complexity: O(n)
+    # space complexity: O(n)
